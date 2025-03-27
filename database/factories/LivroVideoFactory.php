c83bd648606c7f98449840db502cf3400020f970
Henrique Barrel <henriquebarrel@gmail.com>
2025-03-27 14:33:32 -0300

    Add factories

---
+++ database/factories/LivroVideoFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Livro;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class LivroVideoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "livro_id" => Livro::factory(),
+            "path" => fake()->filePath(),
+        ];
+    }
+}
