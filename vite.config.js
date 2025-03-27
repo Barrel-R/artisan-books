1c93a7c98111f2fd2d799534daa78e9aaf1a4800
Henrique Barrel <henriquebarrel@gmail.com>
2025-03-27 09:07:49 -0300

    Setup vite config

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,18 @@ export default defineConfig({
             refresh: true,
         }),
         tailwindcss(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                }
+            }
+        })
     ],
+    resolve: {
+        alias: {
+            vue: "vue/dist/vue.esm-bundler.js"
+        }
+    }
 });
