5ab90e04f6a38f17bff50b9b9d4da8127a949b70
Henrique Barrel <henriquebarrel@gmail.com>
2025-03-27 15:29:56 -0300

    Add requests

---
+++ app/Http/Requests/CategoriaStoreRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoriaStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->can("criar categorias");
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "descricao" => "required|string|min:3|max:255",
+        ];
+    }
+}
---
+++ app/Http/Requests/LivroVideoUpdateRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LivroVideoUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->can("editar videos");
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "livro_id" => "required|exists:livros,id",
+            "path" => "required|string|min:1|max:255",
+        ];
+    }
+}
