612280eaa48ff0a5a377c566b041847513818612
Henrique Barrel <henriquebarrel@gmail.com>
2025-03-27 09:05:34 -0300

    Setup Ziggy

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8" />
+     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+     <title inertia>{{ config('app.name', 'Gatinha Arteira') }}</title>
+     <link rel="preconnect" href="https://fonts.bunny.net">
+     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+     @vite('resources/js/app.js')
+     @routes
+     @inertiaHead
+
+   </head>
+
+   <body>
+     @inertia
+   </body>
+
+ </html>
