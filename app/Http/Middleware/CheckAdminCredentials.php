e2ad4b075742f9f941b87e8148d49ba579d52edf
Henrique Barrel <henriquebarrel@gmail.com>
2025-04-06 14:50:24 -0300

    Fix tailwind and add pt_BR lang

--- app/Http/Middleware/CheckAdminCredentials.php
+++ app/Http/Middleware/CheckAdminCredentials.php
@@ -10,14 +10,14 @@ class CheckAdminCredentials
     public function handle(Request $request, Closure $next)
     {
         // Skip this middleware on login-related routes
-        if ($request->is('admin/login') || $request->is('admin/auth') || $request->is('admin/logout')) {
+        if ($request->is('login') || $request->is('logout')) {
             return $next($request);
         }
 
-        // Check if logged in and session hasn't expired
-        if (!session()->has('admin_logged_in') || !session('admin_logged_in')) {
-            return redirect('/admin/login');
-        }
+        // // Check if logged in and session hasn't expired
+        // if (!session()->has('admin_logged_in') || !session('admin_logged_in')) {
+        //     return redirect('/login');
+        // }
 
         return $next($request);
     }
